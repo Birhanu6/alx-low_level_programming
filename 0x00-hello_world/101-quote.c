476d2a0a04e2bd0212e4f4f725281360c662afed
Birhanu6 <birhanusisayh@gmail.com>
2022-09-08 02:42:55 -0700

    task 8 101 updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Entry Point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-	return (1);
+
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (1);
 }
