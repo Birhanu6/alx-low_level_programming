ff9735ed5f009716aecb10cb3dc442a28cf577da
Birhanu6 <birhanusisayh@gmail.com>
2022-09-09 00:54:51 -0700

    task 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ *main - print all possible combinations of single-digit numbers
+ *
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+return (0);
+}
