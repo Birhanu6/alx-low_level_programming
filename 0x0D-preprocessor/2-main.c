891903d8dd63df6e74505ef7e27697dcb5cc6c19
Birhanu6 <birhanusisayh@gmail.com>
2022-10-10 00:01:20 -0700

    all taskes aresolved

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - check the code
+  *
+  * Return: Always 0.
+  * program that prints the name of the file it was compiled from
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+#ifndef ABS_A
+#define ABS_A
+#define ABS(x) ((x) < 0 ? (x) * -1 : (x))
+#endif
+
